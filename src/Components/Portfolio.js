9f2254e80d35dde6228d77c27a5a25c7f38bbdf1
Evan Hofmeister <evan.hofmeister@gmail.comcd>
2022-05-04 15:26:55 -0500

    Update Contact

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -10,19 +10,19 @@ class Portfolio extends Component {
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
               <div className="article-wrapper">
                 <article className="col-12 col-12-xsmall work-item">
-                  <a href="https://github.com/rammazzoti2000/final-capstone" className="image fit thumb" target="_blank" rel="noopener noreferrer"><img src="images/thumbs/06.png" alt="final-capstone" /></a>
+                  <a href="https://github.com/EvanHofmeister/cryptocurrency-tracker-app" className="image fit thumb" target="_blank" rel="noopener noreferrer"><img src="images/thumbs/Crypto_Dashboard_img.JPG" alt="final-capstone" /></a>
                   <div className="art-description">
                     <div className="text-wrapper">
-                      <h3>Power Tracker WebApp</h3>
-                      <p>App that tracks domestic power consumption. It can register measurements across rooms in the house. The measurements will be accessible in a separate page and will represent useful information on total power consuming, the available units left and what has been saved in percentages.</p>
+                      <h3>Cryptocurrency Market Tracker Dashboard</h3>
+                      <p>Application built to track trends in cryptocurrency prices, derivatives, exchanges and indexes.</p>
                     </div>
                     <ul className="actions">
                       <div className="actions-wrapper">
                         <li><a href="https://final-capstonejx.herokuapp.com/" className="button project-button" target="_blank" rel="noopener noreferrer">View Demo</a></li>
-                        <li><a href="https://github.com/rammazzoti2000/final-capstone" className="button project-button brands" target="_blank" rel="noopener noreferrer"><i className="fa fa-github icon"></i>View Source</a></li>
+                        <li><a href="https://github.com/EvanHofmeister/cryptocurrency-tracker-app" className="button project-button brands" target="_blank" rel="noopener noreferrer"><i className="fa fa-github icon"></i>View Source</a></li>
                       </div>
                       <div className="languages">
-                        <i className="devicon-rails-plain-wordmark technology"></i>
+                        <i className="devicon-javascript-plain technology"></i>
                         <i className="devicon-react-plain-wordmark technology"></i>
                       </div>
                     </ul>
@@ -31,19 +31,18 @@ class Portfolio extends Component {
               </div>
               <div className="article-wrapper">
                 <article className="col-12 col-12-xsmall work-item">
-                  <a href="https://github.com/rammazzoti2000/pic-some-react" className="image fit thumb" target="_blank" rel="noopener noreferrer"><img src="images/thumbs/08.png" alt="pic-some" /></a>
+                  <a href="https://github.com/EvanHofmeister" className="image fit thumb" target="_blank" rel="noopener noreferrer"><img src="images/thumbs/Lidar_Project_img.JPG" alt="pic-some" /></a>
                   <div className="art-description">
                     <div className="text-wrapper">
                       <h3>PicSome WebApp</h3>
-                      <p>A small mock e-commerce website. The idea is for someone to visit the website, look at the images available and choose some of them to have them, theoretically, printed and shipped.</p>
+                      <p>Created reliefs of the NYC metro from Lidar data using computer vision, machine learning and ArcGis tools</p>
                     </div>
                     <ul className="actions">
                       <div className="actions-wrapper">
                         <li><a href="https://pic-somejsx.herokuapp.com/" className="button project-button" target="_blank" rel="noopener noreferrer">View Demo</a></li>
-                        <li><a href="https://github.com/rammazzoti2000/pic-some-react" className="button project-button brands" target="_blank" rel="noopener noreferrer"><i className="fa fa-github icon"></i>View Source</a></li>
+                        <li><a href="https://github.com/EvanHofmeister" className="button project-button brands" target="_blank" rel="noopener noreferrer"><i className="fa fa-github icon"></i>View Source</a></li>
                       </div>
                       <div className="languages">
-                        <i className="devicon-react-plain-wordmark technology"></i>
                       </div>
                     </ul>
                   </div>
